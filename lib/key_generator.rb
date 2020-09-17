ec87d6b24c075813d9541fbe84cf26c7229a3f07
Joshua Carey <jdcarey128@gmail.com>
2020-09-17 16:53:44 -0600

    Structure assign digits to keys test

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,8 +1,12 @@
 class KeyGenerator
-  attr_reader :key 
+  attr_reader :key
 
   def initialize(key)
     @key = key
+    @a_key = ""
+    @b_key = ""
+    @c_key = ""
+    @d_key = ""
   end
 
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,6 +8,14 @@ class KeyGeneratorTest < Minitest::Test
     assert_equal "02794", key_1.key
   end
 
+  def test_it_can_assign_digits_to_shit_keys
+    key_1 = KeyGenerator.new("02794")
+    assert_equal "02", key_1.a_key
+    assert_equal "27", key_1.b_key
+    assert_equal "79", key_1.c_key
+    assert_equal "94", key_1.d_key
+  end
+
 
 
 end
