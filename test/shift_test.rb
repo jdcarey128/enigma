6f66b0ce41cc3e6a2577cbe35eacdc87b3a87526
Joshua Carey <jdcarey128@gmail.com>
2020-09-17 21:56:40 -0600

    Add shift values test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -7,7 +7,26 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
     assert_equal "hello world", shift.message
     assert_equal "02794", shift.key
-    assert_equal "170920", shift.date 
+    assert_equal "170920", shift.date
+  end
+
+  def test_it_can_combine_keys_and_offsets_for_shift_value
+    shift = Shift.new("hello world", "02794", "170920")
+    expected = {
+      "a" => "2",
+      "b" => "31",
+      "c" => "79",
+      "d" => "94"
+    }
+    assert_equal expected, shift.shift_values
+    shift_2 = Shift.new("hello world", "02794", "160920")
+    expected = {
+      "a" => "8",
+      "b" => "31",
+      "c" => "79",
+      "d" => "94"
+    }
+    assert_equal expected, shift_2.shift_values
   end
 
 
