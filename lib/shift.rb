f7f41e9dbf6636ecfdfd9d072c9bb5629d8cb047
Joshua Carey <jdcarey128@gmail.com>
2020-09-18 08:23:54 -0600

    Add rake file

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,10 +1,14 @@
 class Shift
-  attr_reader :message, :key, :date 
+  attr_reader :message, :key, :date
 
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
+  end
+
+  def shift_values
+
   end
 
 end
