53d58d5ab25b30b093b392ed31b80915c6581c90
Joshua Carey <jdcarey128@gmail.com>
2020-09-19 10:57:44 -0600

    Refactor encrypt to pass without date argument

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,19 +1,27 @@
+require './lib/key_generator'
+require './lib/offset_generator'
+require './lib/shift'
+
 class Enigma
   attr_reader :encrypted_message,
               :decrypted_message,
               :key,
               :date
 
-  def initialize(encrypted_message = '', decrypted_message = '',
-                key = '', date = '')
+  def initialize
+    # (encrypted_message = nil, decrypted_message = nil, key = nil, date = nil)
     @encrypted_message = encrypted_message
     @decrypted_message = decrypted_message
     @key = key
     @date = date
   end
 
-  def encrypt(message, key, date)
-    shift = Shift.new(message, key, date)
+  def encrypt(message, key = @key, date = @date)
+    if date == nil
+      shift = Shift.new(message, key)
+    else
+      shift = Shift.new(message, key, date)
+    end
     { encryption: shift.encrypt_message.join, key: shift.key, date: shift.date }
   end
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,10 +5,10 @@ class EnigmaTest < Minitest::Test
   def test_it_exists_and_has_attributes
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
-    assert_equal '', enigma.encrypted_message
-    assert_equal '', enigma.decrypted_message
-    assert_equal '', enigma.key
-    assert_equal '', enigma.date
+    assert_nil enigma.encrypted_message
+    assert_nil enigma.decrypted_message
+    assert_nil enigma.key
+    assert_nil enigma.date
   end
 
   def test_it_can_encrypt_message_with_key_and_date
@@ -29,6 +29,17 @@ class EnigmaTest < Minitest::Test
         date: "040895"
       }
     assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_message_with_only_key_aaa
+    enigma = Enigma.new
+    Date.stubs(:today).returns(Date.new(1995,8,4))
+    expected =  {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+    assert_equal expected, enigma.encrypt("hello world", "02715")
   end
 
 end
