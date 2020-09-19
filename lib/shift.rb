f9504f2b9c36643baafaacdf94b825bed7f153c1
Joshua Carey <jdcarey128@gmail.com>
2020-09-18 20:42:20 -0600

    Refactor encrypt message with shift reducer and prune message helpers

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,12 +3,12 @@ require './lib/offset_generator'
 require './lib/splitaccumulatable'
 
 class Shift
-  attr_reader :message, :key, :date, :alphabet, :stripped_message
+  attr_reader :message, :key, :date, :alphabet, :split_message
   include Splitaccumulatable
 
   def initialize(message, key = KeyGenerator.new, date = OffsetGenerator.new)
     @message = message
-    @stripped_message = splitter(message)
+    @split_message = splitter(message)
     @key = key
     @date = date
     @shift_values = shift_values()
@@ -29,12 +29,15 @@ class Shift
     end
   end
 
-
-
-  def encrypt_letter(letter, shift_value)
-    until shift_value <= 27
+  def shift_reducer(shift_value)
+    until shift_value < 27
       shift_value -= 27
     end
+    shift_value
+  end
+
+  def encrypt_letter(letter, shift_value)
+    shift_value = shift_reducer(shift_value)
     if (@alphabet.index(letter) + shift_value) > 27
       letter = @alphabet.fetch(@alphabet.index(letter) + shift_value - 27)
     else
@@ -43,14 +46,26 @@ class Shift
     letter
   end
 
+  def prune_message(message, count, message_length)
+    (count - message_length).times do
+      message.pop
+    end
+    message
+  end
+
   def encrypt_message
     count = 0
-    message = @stripped_message
-    until count == @stripped_message.count
-      @shift_values.each do |shift, value|
-        require "pry"; binding.pry
+    message = @split_message
+    new_message = []
+    until count >= @split_message.count
+      # require "pry"; binding.pry
+      @shift_values.map do |shift, value|
+        new_message << encrypt_letter(@split_message[0], value.to_i)
+        count += 1
+        @split_message.rotate!
       end
     end
+    prune_message(new_message, new_message.count, @split_message.count)
   end
 
 end
